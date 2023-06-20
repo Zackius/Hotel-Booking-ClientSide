c5022aa3d75c777533454c093bea6a5bdbf05615
Zacharia <ndunguzachary24@gmail.com>
2023-06-20 10:22:38 +0300

    Added image to banner

--- src/Components /banner/index.jsx
+++ src/Components /banner/index.jsx
@@ -1,15 +1,23 @@
 import { useTheme } from "@emotion/react";
-import { useMediaQuery } from "@mui/material";
+import { Typography, useMediaQuery } from "@mui/material";
+import accra from "./"
 import React from "react";
-import { BannerContainer } from "../../styles/banner";
+import { BannerContainer , BannerContent, BannerDescription, BannerImge, BannerTittle} from "../../styles/banner";
 
 const Banner = () => {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <BannerContainer>
+      <BannerImge src="/images/accra.jpg"/>
       <BannerContent>
-<Typography></Typography>
+        <Typography>Bnanner</Typography>
+        <BannerTittle variant= "h2">
+          Hotels
+        </BannerTittle>
+        <BannerDescription variant="subtitle">
+          lorem ipdsum dollar sit amet cosnsecutre adisping eliting do eiusmo tempor incidentfjdj lasd 
+        </BannerDescription>
  </BannerContent>
     </BannerContainer>
   );
--- src/styles/banner/index.js
+++ src/styles/banner/index.js
@@ -7,7 +7,7 @@ export const BannerContainer = styled(Box)(({ theme }) => ({
   display: "flex",
   justifyContent: "center",
   width: "100%",
-  height: "100px",
+  height: "100%",
   padding: "opx opx",
   background: Colors.light_gray,
   [theme.breakpoints.down("sm")]: {
@@ -15,16 +15,41 @@ export const BannerContainer = styled(Box)(({ theme }) => ({
     alignItems: "center",
   },
 }));
+export const BannerImge = styled("img")(({ src, theme }) => ({
+  src: `url(${src})`,
+  width: "500px",
+  [theme.breakpoints.down("md")]: {
+    width: "350px",
+  },
+  [theme.breakpoints.down("sm")]: {
+    width: "320px",
+    height: "300px",
+  },
+}));
 export const BannerContent = styled(Box)(() => ({
-    display: 'flex', 
-    flexDirection: 'column',
-    justifyContent: 'center', 
-    maxWidth: 420,
-    padding: '30px'
-}))
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  maxWidth: 420,
+  padding: "30px",
+}));
 
-export const BannerTittle = styled(Typography)(() = ({
-    lineHeight: 1.5,
-    fontSize: '72px', 
-    marginBottom: '20px'
-}))
+export const BannerTittle = styled(Typography)(({ theme }) => ({
+  lineHeight: 1.5,
+  fontSize: "72px",
+  marginBottom: "20px",
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "42px",
+  },
+}));
+
+export const BannerDescription = styled(Typography)(({ theme }) => ({
+  lineHeight: 1.25,
+  letterSpacing: 1.25,
+  marginBottom: "3m",
+  [theme.breakpoints.down("md")]: {
+    lineHeight: 1.15,
+    letterSpacing: 1.15,
+    marginBottom: "1.5em",
+  },
+}));
