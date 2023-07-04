0d1b6d10b154f9fd0ce88548950e32fdb1a3ed7f
Zacharia <ndunguzachary24@gmail.com>
2023-07-04 23:24:48 +0300

    Added Hotel Data

---
+++ src/data/index.js
@@ -0,0 +1,150 @@
+export const products = [
+  {
+    id: 1,
+    name: "Grand Plaza Hotel",
+    type: "Luxury",
+    city: "New York city",
+    address: "123 Main Street",
+    distance: "2.5 miles",
+    photo:
+      "https://img.freepik.com/free-photo/beautiful-luxury-outdoor-swimming-pool-hotel-resort_74190-7433.jpg",
+    title: "Experience ultimate luxury at Grand Plaza Hotel",
+    description:
+      " Indulge in opulent accommodations and world-class amenities at our luxurious hotel in the heart of New York city.",
+    rating: 4.7,
+  },
+
+  {
+    id: 2,
+    name: "Seaside Resort",
+    type: "Beachfront",
+    city: "Miami",
+    address: "456 Ocean Avenue",
+    distance: "0.1 miles",
+    photo:
+      "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90ZWxzfGVufDB8fDB8fHww&w=1000&q=80",
+    title: "Unwind in paradise at Seaside Resort",
+    description:
+      "Enjoy breathtaking ocean views, white sandy beaches, and a serene atmosphere at our beachfront resort in Miami.",
+    rating: 4.5,
+  },
+  {
+    id: 3,
+    name: "Mountain Lodge Retreat",
+    type: "Wilderness",
+    city: "Aspen",
+    address: "789 Forest Road",
+    distance: "5 miles",
+    photo:
+      "https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?cs=srgb&dl=pexels-pixabay-258154.jpg&fm=jpg",
+    title: "Escape to nature at Mountain Lodge Retreat",
+    description:
+      " Immerse yourself in the beauty of the mountains, experience outdoor adventures, and relax in cozy cabins at our wilderness retreat in Aspen.",
+    rating: 4.8,
+  },
+
+  {
+    id: 4,
+    name: "city Lights Hotel",
+    type: "Urban",
+    city: "London",
+    address: " 10 Piccadilly Circus",
+    distance: " 0.3 miles",
+    photo:
+      "https://thehoughtonhotel.com/wp-content/uploads/2023/02/Houghton-Hotel-3-20-scaled.jpg",
+    title: "Discover the vibrant city at city Lights Hotel",
+    description:
+      " Immerse yourself in the energy of London's bustling streets, explore iconic landmarks, and experience the vibrant nightlife from our urban hotel.",
+    rating: 4.4,
+  },
+
+  {
+    id: 5,
+    name: "Tranquil Spa Resort",
+    type: "Wellness",
+    city: "Bali",
+    address: "321 Serenity Lane",
+    distance: "1 mile",
+    photo:
+      "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90ZWx8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+    title: "Rejuvenate your mind and body at Tranquil Spa Resort",
+    description:
+      "Experience ultimate relaxation with rejuvenating spa treatments, yoga retreats, and tranquil surroundings at our wellness resort in Bali.",
+    rating: 4.9,
+  },
+
+  {
+    id: 6,
+    name: " Desert Oasis Hotel",
+    type: "Desert",
+    city: " Dubai",
+    address: "555 Sand Dune Street",
+    distance: " 4 miles",
+    photo:
+      "https://dubai-experience.com/wp-content/uploads/2019/08/wuestenhotel-al-maha-desert-resort.jpg",
+    title: " Experience luxury in the desert at Desert Oasis Hotel",
+    description:
+      "Discover the allure of the Arabian desert, indulge in luxurious accommodations, and enjoy unparalleled hospitality at our desert hotel in Dubai.",
+    rating: 4.6,
+  },
+  {
+    id: 7,
+    name: "Historic Mansion Inn",
+    type: "Boutique",
+    city: "Charleston",
+    address: "777 Heritage Lane",
+    distance: "0.2 miles",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmJC6gBrZ9t0B__H-xk5jt5TILbFq6WLyvDrH8AekEhuIuN2UMP52FBWvTSGrFUv4Oceo&usqp=CAU",
+    title: "Step back in time at Historic Mansion Inn",
+    description:
+      "Immerse yourself in the charm of a bygone era, experience southern hospitality, and admire the architectural beauty of our boutique hotel in Charleston.",
+    rating: 4.7,
+  },
+
+  {
+    id: 8,
+    name: "Ski Chalet Resort",
+    type: "Ski Resort",
+    city: "Whistler",
+    address: "999 Snowy Peak Road",
+    distance: "0.5 miles",
+    photo:
+      "https://www.travelandleisure.com/thmb/OiDnPGo3k9QLRT9__TPhFZcr7PU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/rosewood-carlyle-presidential-suite-LUXESUITE0122-0046808a88924e57922d78c7f1d9ca60.jpg",
+
+    title: "Embrace winter adventures at Ski Chalet Resort",
+    description:
+      "Hit the slopes, enjoy cozy alpine accommodations, and unwind in hot tubs with stunning mountain views at our ski resort in Whistler.",
+    rating: 4.8,
+  },
+
+  {
+    id: 9,
+    name: "Tropical Paradise Inn",
+    type: "Island",
+    city: "Maldives",
+    address: "777 Palm Beach",
+    distance: "0.1 miles",
+    photo:
+      "https://img.freepik.com/free-photo/beautiful-luxury-outdoor-swimming-pool-hotel-resort_74190-7433.jpg?w=2000",
+    title: " Experience a tropical paradise at Tropical Paradise Inn",
+    description:
+      "Immerse yourself in crystal-clear waters, relax on pristine beaches, and savor the beauty of the Maldives at our idyllic island retreat.",
+    rating: "4.9",
+  },
+
+  {
+    id: 10,
+    name: "Riverside Lodge",
+    type: "Countryside",
+    city: "Zurich",
+    address: " 111 Riverbank Road",
+    distance: "1.5 miles",
+    photo:
+      "https://upgradedpoints.com/wp-content/uploads/2023/03/Crowne-Plaza-Resort-Guam-pool.jpg",
+    title: " Experience tranquility at Riverside Lodge",
+    description:
+      "Surrounded by picturesque landscapes, enjoy peaceful walks along the river, and indulge in Swiss hospitality at our charming countryside lodge in Zurich.",
+    rating: 4.5,
+  },
+];
